174be92a510a881b604698ad896712653b72e7b4
poferri <poferipoferi@yahoo.com>
2019-07-14 00:31:09 -0500

    initial commit

--- apps/portfolio/urls.py
+++ apps/portfolio/urls.py
@@ -1,12 +1,27 @@
 from django.conf.urls import url
+from django.contrib import admin
 from . import views  # This line is new!
+from .views import Pdf
+from .views import wishmaker
 
 urlpatterns = [
     url(r"^$", views.main),
-    url(r"^index/$", views.index, name="index"),
-    url(r"^pyth/$", views.pyth, name="pyth"),
-    url(r"^sharp/$", views.sharp, name="sharp"),
+    url(r"^home/$", views.home, name="home"),
+    url(r"^python/$", views.python, name="python"),
+    url(r"^csharp/$", views.csharp, name="csharp"),
     url(r"^about/$", views.about, name="about"),
+    url(r"^about/Pdf/$", views.Pdf, name="Pdf"),
+    url(r"^wishmaker/$", views.wishmaker, name="wishmaker"),
+    url(r"^wishmakersite/$", views.wishmakersite, name="wishmakersite"),
+    url(r"^quotedash/$", views.quotedash, name="quotedash"),
+    url(r"^quotedashsite/$", views.quotedashsite, name="quotedashsite"),
+    url(r"^contact/$", views.contact, name="contact"),
+    url(r"^clakkle/$", views.clakkle, name="clakkle"),
+    url(r"^weddingplanner/$", views.weddingplanner, name="weddingplanner"),
+    url(r"^weddingplannersite/$", views.weddingplannersite, name="weddingplannersite"),
+    url(r"^healthwall/$", views.healthwall, name="healthwall"),
+    url(r"^healthwallsite/$", views.healthwallsite, name="healthwallsite"),
+    url(r"^linkedIn/$", views.linkedIn, name="linkedIn"),
     # This line has changed! Notice that urlpatterns is a list, the comma is in
 ]  # anticipation of all the routes that will be coming soon
 
--- apps/portfolio/views.py
+++ apps/portfolio/views.py
@@ -1,23 +1,89 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.template.loader import get_template
+from django.http import HttpResponse
+from django.views.generic import View
 
-# the index function is called when root is visited
+from portfolioproject.utils import render_to_pdf
 
 
 def main(req):
-    return render(req, "portfolio/consulting.html")
+    return redirect("portfolio:home")
 
 
-def index(req):
-    return render(req, "portfolio/consulting.html")
+def home(req):
+    return render(req, "portfolio/home.html")
 
 
-def pyth(req):
+def python(req):
     return render(req, "portfolio/python.html")
 
 
-def sharp(req):
-    return render(req, "portfolio/aspnet.html")
+def csharp(req):
+    return render(req, "portfolio/csharp.html")
 
 
 def about(req):
     return render(req, "portfolio/about.html")
+
+
+def contact(req):
+    return redirect("portfolio/about/Pdf")
+
+
+def Pdf(request, *args, **kwargs):
+    pdf = render_to_pdf("portfolio/resume.html")
+    if pdf:
+        response = HttpResponse(pdf, content_type="application/pdf")
+        filename = "resume_%s.pdf" % ("Elvis_Makia")
+        content = "inline; filename='%s'" % (filename)
+        download = request.GET.get("download")
+        if download:
+            content = "attachment; filename='%s'" % (filename)
+        response["Content-Disposition"] = content
+        return response
+    return HttpResponse("Not found")
+
+
+def wishmaker(req):
+    return redirect("https://github.com/poferri/wishmaker.git")
+
+
+def wishmakersite(req):
+    return redirect("http://18.217.90.11/")
+
+
+def quotedash(req):
+    return redirect("https://github.com/poferri/quote_dash.git")
+
+
+def quotedashsite(req):
+    return redirect("http://52.14.128.79/")
+
+
+def clakkle(req):
+    return redirect("https://github.com/poferri/clakkle.git")
+
+
+def clakklesite(req):
+    return redirect("http://3.14.136.63/")
+
+
+def weddingplanner(req):
+    return redirect("https://github.com/poferri/WeddingPlanner.git")
+
+
+def weddingplannersite(req):
+    return redirect("http://18.222.165.8/")
+
+
+def healthwall(req):
+    return redirect("https://github.com/poferri/secrets.git")
+
+
+def healthwallsite(req):
+    return redirect("http://18.220.54.169/")
+
+
+def linkedIn(req):
+    return redirect("https://www.linkedin.com/in/elvismakia/")
+
